982cfc47af7805cfb1b1b79b51bf7a8e737b3931
Sk-08-s <2k23.csdsc2311176@gmail.com>
2025-08-11 17:18:01 +0000

    data types

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version 
+//alert("hello") undefined error 
+ let name =" Sahil" //string
+ let age = 18 // numeric
+ let is = false //boolean 
+ console.log(typeof age)
+ /* number 
+  bigint
+  string = ""
+  boolean = true/false
+  null = standalone value
+  undefined ==
+  symbol = unique 
+
+  object
+  */
---
+++ basics_01/0_1basics.js
@@ -0,0 +1,7 @@
+const accounId = 144453
+let accountEmail = "sk@gmail.com"
+var accountpassword = "1234"
+accountCity = "Jaipur"
+let acpt //undefined 
+console.log(accounId);
+console.table([accounId,accountEmail,accountpassword,accountCity,acpt])
